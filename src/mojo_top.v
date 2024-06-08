8c5011ddb1eb2579cc1aa21a1d570da64d9a8ebc
Eric Jessee <ericjessee@gmail.com>
2024-06-07 23:51:30 -0400

    first crack at loopback test

--- src/mojo_top.v
+++ src/mojo_top.v
@@ -21,6 +21,30 @@ module mojo_top(
     output avr_tx_debug,
     output avr_rx_debug,
     output avr_rx_busy_debug
+
+    //audio system clk from pll
+    input i_scki;
+
+    //adc signals
+    output o_adc_fmt;
+    output o_adc_md1;
+    output o_adc_md2;
+    input  i_acd_adata;
+    input  i_adc_bck;
+    input  i_adc_lrck;
+
+    //dac signals
+    output o_dac_nmute;
+    output o_dac_adata;
+    output o_dac_bck;
+    output o_dac_lrck;
+
+    //pll signals
+    output o_pll_csel;
+    output o_pll_fs1;
+    output o_pll_fs2;
+    output o_pll_sr;
+
 );
 
   wire rst = ~rst_n; // make reset active high
@@ -32,6 +56,23 @@ module mojo_top(
   wire tx_busy;
   wire [7:0] rx_data;
   wire new_rx_data;
- 
+
+  //INITIAL TEST hard wire control signals
+  //also loopback input to output
+  //control signals:
+  assign o_adc_fmt = 1'b0;
+  assign o_adc_md1 = 1'b1;
+  assign o_adc_md2 = 1'b1;
+  assign o_dac_nmute = 1'b1;
+  assign o_pll_fs1 = 1'b0;
+  assign o_pll_fs1 = 1'b0;
+  assign o_pll_fs1 = 1'b0;
+  
+  //digital audio signals
+  assign o_dac_adata = i_adc_adata;
+  assign o_dac_bck = i_adc_bck;
+  assign o_dac_lrck = ~i_adc_lrck; //dac and adc lrcks are opposite
+
+
 endmodule
 
