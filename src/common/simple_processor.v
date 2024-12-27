f15f6efae17496378a5c6a77de571e1e36694511
Eric Jessee <ericjessee@gmail.com>
2024-12-27 16:16:55 -0700

    initial thoughts for process module, output buffers

---
+++ src/common/simple_processor.v
@@ -0,0 +1,60 @@
+module simple_processor
+#(
+    parameter SAMPLE_SIZE=24,
+    parameter IO_BUFF_SIZE=64,
+    parameter IO_BUFF_PTR_BITS = $clog2(IO_BUFF_SIZE)
+)
+(
+)
+(
+    input clk,
+    input rst,
+
+    input chunk_pulse;
+
+    //input buffer memory interface
+    output reg [IO_BUFF_PTR_BITS-1:0] input_buff_ptr;
+    input      [SAMPLE_SIZE-1:0]      input_buff_sample;
+
+    //output buffer memory interface
+    output reg [IO_BUFF_PTR_BITS-1:0] output_buff_ptr;
+    output     [SAMPLE_SIZE-1:0]      output_buff_sample;
+);
+
+wire filter_output_ready;
+wire filter_ready_for_data;
+
+wire [39:0] filtered_data;
+//probably incorrect truncation
+assign output_buff_sample = filtered_data[23:0];
+
+filter filter_0(
+    .rfd(filter_ready_for_data),
+    .rdy(filter_output_ready),
+    .clk(clk),
+    .dout(filtered_data),
+    .din(input_buff_sample)
+);
+
+simple_ram finished_buffer(
+
+);
+
+reg start_pulse;
+reg done_pulse;
+always @(posedge clk) begin
+    if (chunk_pulse) begin
+        input_buff_ptr <= 0;
+        start_pulse <= 1;
+    end
+    if (start_pulse) begin
+        start_pulse <= 0;
+    end else if (input_buff_ptr >= I_BUFF_SIZE-1) begin
+        done_pulse <= 1;
+    end else begin
+        input_buff_ptr <= input_buff_ptr + 1;
+    end
+end
+
+
+endmodule
--- src/mojo_top.v
+++ src/mojo_top.v
@@ -37,8 +37,6 @@ module mojo_top(
  
   assign led = 8'b0;
 
-  //INITIAL TEST hard wire control signals
-  //also loopback input to output
   //control signals:
   assign o_adc_fmt = 1'b0;
   assign o_adc_md1 = 1'b1;
@@ -75,15 +73,13 @@ module mojo_top(
   reg r_rx_buff_flush;
   reg r_rx_flush_done;
 
-  reg [IO_BUFF_PTR_BITS-1:0] l_tx_buff_ptr;
-  wire signed [SAMPLE_SIZE-1:0] l_tx_sample;
-
-  reg [IO_BUFF_PTR_BITS-1:0] l_rx_buff_flush_ptr;
-  wire signed [SAMPLE_SIZE-1:0] l_rx_flush_sample;
-
-
-
-  wire signed [SAMPLE_SIZE-1:0] r_tx_sample;
+  i2s_rx #(.WORD_SIZE(WORD_SIZE))i2s_rx_0(
+    .bck(i_adc_bck),
+    .lrck(i_adc_lrck),
+    .din(i_adc_adata),
+    .l_dout(l_rx_data),
+    .r_dout(r_rx_data)
+  );
 
   simple_ram l_rx_buffer(
     .clka(i_adc_bck),
@@ -151,68 +147,39 @@ module mojo_top(
     end
   end
 
-  wire [SAMPLE_SIZE-1:0] read_sample;
-  //process FIFO
-  buffer_fifo #(
-    .SAMPLE_SIZE(SAMPLE_SIZE),
-    .IO_BUFF_SIZE(IO_BUFF_SIZE),
-    .FIFO_DEPTH(16)
-  ) l_rx_fifo(
-    .clk(i_adc_bck),
-    .rst(rst),
-    .fill(l_rx_buff_flush),
-    .fill_done(l_rx_flush_done),
-    .read(1'b0),
-    .read_done(1'b0),
-    .write_sample(l_rx_flush_sample),
-    .write_ptr(l_rx_buff_flush_ptr),
-    .read_sample(read_sample),
-    .read_ptr(6'b1)
+  //TX section
+  wire tx_buff_sel;
+  wire [31:0] l_tx_word;
+  wire [23:0] l_tx_sample_0;
+  wire [23:0] l_tx_sample_1;
+  assign l_tx_word = tx_buff_sel ? {l_tx_sample_0, 8'b0} : {l_tx_sample_1, 8'b0};
+
+  simple_ram l_tx_buffer_0(
+    .clka(i_adc_bck),
+    .wea(tx_buff_sel),
+    .addra(),
+    .dina(),
+    .clkb(),
+    .addrb(),
+    .doutb(l_tx_sample_0)
   );
 
-  i2s_rx #(.WORD_SIZE(WORD_SIZE))i2s_rx_0(
-    .bck(i_adc_bck),
-    .lrck(i_adc_lrck),
-    .din(i_adc_adata),
-    .l_dout(l_rx_data),
-    .r_dout(r_rx_data)
+  simple_ram l_tx_buffer_1(
+    .clka(i_adc_bck),
+    .wea(!tx_buff_sel),
+    .addra(),
+    .dina(),
+    .clkb(),
+    .addrb(),
+    .doutb(l_tx_sample_1)
   );
 
-  //generating the I2S TX signals
-  //this should be moved into its own module eventually
-  reg        [4:0]          word_cnt;
-  reg                        tx_lrck;
-  reg signed [WORD_SIZE-1:0] tx_word_l;
-  reg signed [WORD_SIZE-1:0] tx_word_r;
-
-  assign o_dac_bck = i_adc_bck; //use unified clock
-  assign o_dac_lrck = tx_lrck;
-
-  always @(negedge i_adc_bck) begin
-    if (rst) begin
-        word_cnt <= 0;
-        tx_lrck <= 0;
-        tx_word_l <= 0;
-        tx_word_r <= 0;
-    end else if (word_cnt == WORD_SIZE-1) begin
-        word_cnt <= 0;
-        tx_lrck <= ~tx_lrck;
-        if (tx_lrck) begin
-            tx_word_l <= l_tx_sample;
-        end else begin
-            tx_word_r <= read_sample;
-        end
-    end else begin
-        word_cnt <= word_cnt + 5'b1;
-        l_tx_buff_ptr <= word_cnt;
-    end
-end
   
   i2s_tx #(.WORD_SIZE(32))i2s_tx_0(
     .bck(i_adc_bck),
-    .lrck(tx_lrck),
-    .l_din(tx_word_l),
-    .r_din(tx_word_r),
+    .lrck(i_adc_lrck),
+    .l_din(),
+    .r_din(),
     .dout(o_dac_adata)
   );
 
